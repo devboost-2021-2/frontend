a42d8704723d7da69a130207904aa4cf0ee5f7e7
Bruno Akamine <bruno.akamine13@gmail.com>
2021-09-19 19:48:03 -0300

    Init question page

---
+++ src/pages/QuestionPage/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function QuestionPage() {
+  return (
+    <div>
+        <h1>Questão X)</h1>
+        <input type = "radio" id = "a">
+        <label for = "a">alternativa A</label>
+    </div>
+  );
+}
+
+export default QuestionPage;
