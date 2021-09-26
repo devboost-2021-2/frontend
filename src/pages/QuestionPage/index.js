5f2e23874c6920cfe29adddc2c9572dd0f77bd7e
joaogaaj <joao.gabrielaaj@gmail.com>
2021-09-26 20:28:05 -0300

    Fix panes placement and add timer.
    Signed-off-by: João Gabriel Josephik<joao.gabrielaaj@gmail.com>
    Co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com>
    Co-authored-by: Willian Mori <wnmrmr@gmail.com>
    Co-authored-by: Bruno Akamine <bruno.akamine13@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/pages/QuestionPage/index.js
+++ src/pages/QuestionPage/index.js
@@ -1,28 +1,25 @@
 import "./style.css";
+import React, {useState} from 'react';
+function Cronometro() {
+  //TODO
+  const [timer, setTimer] = useState(7200);
 
-function Links() {
-
-    let rows = []
-    rows.push()
-    for (let i = 1; i <= 20; i++) {
-
-      rows.push(<a></a>)
-    }
-    rows.push()
-    return (
-      <div>
-        <h1>Loop 1:</h1>
-        <ul>{rows}</ul>
-      </div>
-    )
-
-}
-
-function QuestionPage() {
-  const alternatives = [{id:"a",name: "alternativa A"},{id:"b",name: "alternativa B"},{id:"c",name: "alternativa C"}]
-  const questionLinks = Array.from({length: 4}, () => Array.from({length: 5}, () => 0));
+  //Retorna horário no formato HH:MM:SS
+  const formatTime = (time) =>{
+    const seconds = time % 60;
+    var minutes = Math.floor(time / 60);
+    const hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+    return hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0"+seconds).slice(-2);
+  }
+  setTimeout(() => setTimer(timer-1), 1000) //Define atualizador do cronômetro para rodar a cada segundo.
+  return <h2 id='timer'>Timer: {formatTime(timer)}</h2>
   
-
+}
+function QuestionPage() {
+  const alternatives = [{ id: "a", name: "alternativa A" }, { id: "b", name: "alternativa B" }, { id: "c", name: "alternativa C" }]
+  const questionLinks = Array.from({ length: 4 }, () => Array.from({ length: 5 }, () => 0));
+  
   return (
     <div id="containerPrincipal">
       <div id="containerEsquerda">
@@ -30,26 +27,27 @@ function QuestionPage() {
         <p>Vestibular</p>
         <p>Enunciado</p>
         {
-          alternatives.map((alternative) =>(
+          alternatives.map((alternative) => (
             <div>
-              <input type = "radio" id = {alternative.id} name = "Question"/>
-              <label htmlFor = {alternative.id}>{alternative.name}</label>
+              <input type="radio" id={alternative.id} name="Question" />
+              <label htmlFor={alternative.id}>{alternative.name}</label>
             </div>
           ))
         }
       </div>
       <div id="containerDireita">
+        <Cronometro/>
         <h1>Questões</h1>
-          {
+        {
           questionLinks.map((questions) => (
-              {
-                questions.map((question) => (
-                  <button className="circle" id={question}></button>
-                ))
-              }
+            <div>{
+              questions.map((question) => (
+                <button className="circle" id={question}></button>
+              ))
+            }
             </div>
           ))
-          }
+        }
         <button>Finalizar</button>
       </div>
     </div>
