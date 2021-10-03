ac4c7dba3ee2f22e33d683d035161e15e2148d90
Vinicius Frota <viniciuspxfrota@gmail.com>
2021-10-03 20:03:34 -0300

    Change the implementations of the buttons
    Signed-off-by: Vinicius Frota <viniciuspxfrota@gmail.com>
    Co-authored-by: Eike Souza  <eike_souza@hotmail.com>
    Co-authored-by: Willian Mori <wnmrmr@gmail.com>
    Co-authored-by: Bruno Akamine <bruno.akamine13@gmail.com>
    Co-authored-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/pages/QuestionPage/index.js
+++ src/pages/QuestionPage/index.js
@@ -1,66 +1,78 @@
 import "./style.css";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 function Timer() {
   //TODO
   const [timer, setTimer] = useState(7200);
 
   //Retorna horário no formato HH:MM:SS
-  const formatTime = (time) =>{
+  const formatTime = (time) => {
     const seconds = time % 60;
     var minutes = Math.floor(time / 60);
     const hours = Math.floor(minutes / 60);
     minutes = minutes % 60;
-    return hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0"+seconds).slice(-2);
-  }
-  setTimeout(() => setTimer(timer-1), 1000) //Define atualizador do cronômetro para rodar a cada segundo.
-  return <h2 id='timer'>Timer: {formatTime(timer)}</h2>
-  
+    return (
+      hours + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2)
+    );
+  };
+  setTimeout(() => setTimer(timer - 1), 1000); //Define atualizador do cronômetro para rodar a cada segundo.
+  return <h2 id="timer">Timer: {formatTime(timer)}</h2>;
 }
-function Selector(props){
-  return(
-  <>
-    <h1>Questões</h1>
-      {
-        props.questionLinks.map((questions) => (
-          <div>{
-            questions.map((question) => (
-              <button className="circle" id={question}></button>
-            ))
-          }
-          </div>
-        ))
-      }
-  </>
-  )
-
-}
-function Question(props){
+function Selector(props) {
   return (
-  <div id="containerEsquerda">
-    <h1>Questão X)</h1>
-    <p>Vestibular</p>
-    <p>Enunciado</p>
-    {
-      props.alternatives.map((alternative) => (
+    <>
+      <h1>Questões</h1>
+      {props.allQuestions.map((question, index) => (
+        <div class="inline" key={question.number}>
+          {index % props.numberPerLine === 0 && <br />}
+          <button className="circle" id={question.number}></button>
+        </div>
+      ))}
+    </>
+  );
+}
+function Question({ question }) {
+  return (
+    <div id="containerEsquerda">
+      <h1>Questão {question.number})</h1>
+      <p>{question.exam}</p>
+      <p>{question.statement}</p>
+      {question.alternatives.map((alternative) => (
         <div>
           <input type="radio" id={alternative.id} name="Question" />
-          <label htmlFor={alternative.id}>{alternative.name}</label>
+          <label htmlFor={alternative.id}>{alternative.option}</label>
         </div>
-      ))
-    }
-  </div>
-  )
+      ))}
+    </div>
+  );
 }
 function QuestionPage() {
-  const alternatives = [{ id: "a", name: "alternativa A" }, { id: "b", name: "alternativa B" }, { id: "c", name: "alternativa C" }]
-  const questionLinks = Array.from({ length: 4 }, () => Array.from({ length: 5 }, () => 0));
-  
+  const alternatives = [
+    { id: "a", option: "Texto da alternativa A" },
+    { id: "b", option: "Texto da alternativa B" },
+    { id: "c", option: "Texto da alternativa C" },
+    { id: "d", option: "Texto da alternativa D" },
+  ];
+
+  const question = {
+    alternatives: alternatives,
+    number: 1,
+    correctAnswer: "d",
+    statement: "Qual alternativa está certa?? Me diz:",
+    exam: "FUVEST 2030",
+  };
+
+  const allQuestions = [];
+
+  for (let i = 0; i < 90; i++) {
+    allQuestions.push(question);
+  }
   return (
     <div id="containerPrincipal">
-      <Question alternatives = {alternatives}/>  
+      <Question question={question} />
       <div id="containerDireita">
-        <Timer/>
-        <Selector questionLinks = {questionLinks}/>
+        <Timer />
+        <Selector allQuestions={allQuestions} numberPerLine={10} />
+        <br />
         <button>Finalizar</button>
       </div>
     </div>
