fcc7957fb18dd15acad5e32a49d70237997abd72
eike012 <eike_souza@hotmail.com>
2021-10-03 18:54:23 -0300

    Split timer and selector into its own components Signed-off-by: eike012 <eike_souza@hotmail.com> Co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com> Co-authored-by: Willian Mori <wnmrmr@gmail.com> Co-authored-by: Bruno Akamine <bruno.akamine13@gmail.com> Co-authored-by: João Gabriel Josephik <joao.gabrielaaj@gmail.com> Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/pages/QuestionPage/index.js
+++ src/pages/QuestionPage/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import React, {useState} from 'react';
-function Cronometro() {
+function Timer() {
   //TODO
   const [timer, setTimer] = useState(7200);
 
@@ -16,38 +16,51 @@ function Cronometro() {
   return <h2 id='timer'>Timer: {formatTime(timer)}</h2>
   
 }
+function Selector(props){
+  return(
+  <>
+    <h1>Questões</h1>
+      {
+        props.questionLinks.map((questions) => (
+          <div>{
+            questions.map((question) => (
+              <button className="circle" id={question}></button>
+            ))
+          }
+          </div>
+        ))
+      }
+  </>
+  )
+
+}
+function Question(props){
+  return (
+  <div id="containerEsquerda">
+    <h1>Questão X)</h1>
+    <p>Vestibular</p>
+    <p>Enunciado</p>
+    {
+      props.alternatives.map((alternative) => (
+        <div>
+          <input type="radio" id={alternative.id} name="Question" />
+          <label htmlFor={alternative.id}>{alternative.name}</label>
+        </div>
+      ))
+    }
+  </div>
+  )
+}
 function QuestionPage() {
   const alternatives = [{ id: "a", name: "alternativa A" }, { id: "b", name: "alternativa B" }, { id: "c", name: "alternativa C" }]
   const questionLinks = Array.from({ length: 4 }, () => Array.from({ length: 5 }, () => 0));
   
   return (
     <div id="containerPrincipal">
-      <div id="containerEsquerda">
-        <h1>Questão X)</h1>
-        <p>Vestibular</p>
-        <p>Enunciado</p>
-        {
-          alternatives.map((alternative) => (
-            <div>
-              <input type="radio" id={alternative.id} name="Question" />
-              <label htmlFor={alternative.id}>{alternative.name}</label>
-            </div>
-          ))
-        }
-      </div>
+      <Question alternatives = {alternatives}/>  
       <div id="containerDireita">
-        <Cronometro/>
-        <h1>Questões</h1>
-        {
-          questionLinks.map((questions) => (
-            <div>{
-              questions.map((question) => (
-                <button className="circle" id={question}></button>
-              ))
-            }
-            </div>
-          ))
-        }
+        <Timer/>
+        <Selector questionLinks = {questionLinks}/>
         <button>Finalizar</button>
       </div>
     </div>
