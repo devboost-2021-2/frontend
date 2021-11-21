c5ad5b94476463738ae9787a5710a2f3167eb5f6
larissavmss <larissavitoriamedeiros@usp.br>
2021-11-21 20:13:54 -0300

    Add visible password

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,18 +1,29 @@
 import "./style.css";
+import { useState } from "react";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs"
 
 function Login() {
+  const [showpassword, setShowpassword] = useState(false);
   return (
     <div className="login">
       <h1>Login</h1>
       <form>
-        <input
+        <input className="login-input" style={{width:"50%"}}
           type="text"
           placeholder="Email ou nome de usuário"
           name="username"
           required
         />
-        <input type="password" placeholder="Senha" name="password" required />
-        <button type="submit">ENTRAR</button>
+        <div className="login-password">
+          <input className="login-input" style={{width:"100%"}}
+            type="password" 
+            placeholder="Senha" 
+            name="password" 
+            required
+          />
+          <button onClick={setShowpassword(!showpassword)} ><BsFillEyeFill/></button>
+        </div>
+        <button className="login-button" type="submit">ENTRAR</button>
       </form>
       <a href="">Ainda não tem conta? Cadastra-se já</a>
       <a href="">Esqueceu a senha?</a>
